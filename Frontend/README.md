6e30d6eefc8816585081259098158fa47e9eb5fe
Edward B <ebotelloaroca00@gmail.com>
2025-01-16 16:53:59 -0500

    Añaden archivos iniciales para proyecto e inicializacion de dependencias

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,2 +1,8 @@
 <h1>Carpeta para Frontend</h1>
 <img src="https://github.com/user-attachments/assets/a7d0ff35-af40-4706-a3e3-b63a7e4d3004" width="120px">
+
+
+- Vite
+- ReactJS
+- React Router Dom
+- Tailwind CSS
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center text-3xl font-bold  ">
+      QR Share PRO
+    </div>
+  );
+}
+
+export default App;
