e08d9c5be6a7b2765ff1fb2cee38bb8497f83adf
LeandroDziej <leandro_dziej@yahoo.es>
2025-01-22 19:53:43 -0300

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
+<<<<<<< HEAD
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+=======
+>>>>>>> a1a60854f26d1c3ab97fe5f9691aa702aa179713
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center text-3xl font-bold  ">
+   <div>
+    <Navbar/>
+    <div className="h-screen flex justify-center items-center text-3xl font-bold  ">    
       QR Share PRO
     </div>
+  </div>
   );
+  
 }
 
 export default App;
