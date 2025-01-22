1458e7d3b506bfa14a928b47c18fcb792af78462
Edward B <ebotelloaroca00@gmail.com>
2025-01-22 18:22:15 -0500

    Arregla conflicto para poder mostrar navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
-import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 
-=======
->>>>>>> a1a60854f26d1c3ab97fe5f9691aa702aa179713
 function App() {
   return (
-   <div>
-    <Navbar/>
-    <div className="h-screen flex justify-center items-center text-3xl font-bold  ">    
-      QR Share PRO
+    <div>
+      <Navbar />
+      <div className="h-screen flex justify-center items-center text-3xl font-bold  ">
+        QR Share PRO
+      </div>
     </div>
-  </div>
   );
-  
 }
 
 export default App;
