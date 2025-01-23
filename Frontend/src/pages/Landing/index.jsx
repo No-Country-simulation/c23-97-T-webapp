76433606bd288fa5ef3b50e506af2f7a78de0cf8
Edward B <ebotelloaroca00@gmail.com>
2025-01-23 17:45:27 -0500

    se crea componente container

--- Frontend/src/pages/Landing/index.jsx
+++ Frontend/src/pages/Landing/index.jsx
@@ -1,15 +1,16 @@
 import Search from "../../components/Search";
 import CategoryItem from "../../components/CategoryItem";
+import Container from "../../components/Container";
 
 const Landing = () => {
   return (
-    <section className="flex flex-col p-5">
+    <Container>
       <Search />
       <div className="flex flex-wrap justify-center items-center gap-4 my-4">
         <CategoryItem />
         <CategoryItem />
       </div>
-    </section>
+    </Container>
   );
 };
 
