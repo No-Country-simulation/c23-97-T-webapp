1b89c90746284a0721865abd926766487cafb26d
Leandro Eduardo Dziej <leandro_dziej@yahoo.es>
2025-01-25 18:02:22 -0300

    Modificacion footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className="flex flex-col justify-center items-center border border-gray-300 bg-gray-100 py-4">
-        <p className='flex justify-center space-x-4 mt-4'>
+    <div className="flex justify-center items-center border border-gray-300 bg-gray-100 py-4">
+        <p>
             &copy; {new Date().getFullYear()} QR Share Pro. All rights reserved.
         </p>
     </div>
