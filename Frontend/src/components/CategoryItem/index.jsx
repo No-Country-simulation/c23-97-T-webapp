2db5b27ee2007dbb7d4bff93eee2d24f51a926c0
Edward B <ebotelloaroca00@gmail.com>
2025-01-28 17:25:31 -0500

    añade conexion a la API

--- Frontend/src/components/CategoryItem/index.jsx
+++ Frontend/src/components/CategoryItem/index.jsx
@@ -6,7 +6,7 @@ const CategoryItem = ({ category }) => {
       className="text-[#181411] font-bold font-sans text-lg bg-[#f5f2f0] py-1 px-3 rounded-xl hover:scale-105 transition-transform duration-200 hover:font-semibold hover:bg-[#ded4cd]"
       to={`/${category.nombre}`}
     >
-      <h3 className="mb-3">{category.nombre}</h3>
+      <h3 className="mb-3 capitalize">{category.nombre}</h3>
       <p className="text-gray-600 text-sm leading-relaxed">
         {category.descripcion}
       </p>
