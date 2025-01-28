ad1bdae200014743a2002b5414132076e4c74aae
Leandro Eduardo Dziej <leandro_dziej@yahoo.es>
2025-01-28 09:16:08 -0300

    Header resonsive

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,35 +1,94 @@
 import React, {useState} from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
+//import './Navbar.css'
+//import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
-import Listaitem from '../ListaItems/Listaitem.jsx'
+//import Listaitem from '../ListaItems/Listaitem.jsx'
 // Esta parte es el Header de la pagina comun a todas las paginas
 // Aparecen menu, logo, login y compras
 // Falta hacer
 export const Navbar = () => {
   const [menu, setMenu] = useState("home")
   return (
-    <div className='navbar flex-col bg-[#f5f2f0] p-4 flex justify-between items-center px-4 py-4 shadow-sm md:justify-around"'>
-      <div className="container mx-auto flex justify-between items-center">
-      <div className=" text-2xl text-[#181411] font-bold">QR Share Pro</div>
-        <ul className='nav-menu hidden md:flex gap-4'>
+    <div className='border border-gray-300 bg-[#f5f2f0] shadow-sm'>
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center p-4">
+      <div className="text-2xl text-[#181411] font-bold">QR Share Pro</div>
+      {/* Navigation Menu */}
+        <ul className='hidden md:flex gap-6 text-lg text-[#181411] font-medium'>
 {/*         <Listaitem>Shop</Listaitem>
             <Listaitem>Men</Listaitem>
             <Listaitem>Woman</Listaitem>
             <Listaitem>Kids</Listaitem>*/}
-            <li onClick={()=>{setMenu("home")}} className="hover:text-[#8a7560] cursor-pointer">Home{menu==="home"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("about_us")}} className="hover:text-[#8a7560] cursor-pointer">About us{menu==="about_us"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("services")}} className="hover:text-[#8a7560] cursor-pointer">Services{menu==="services"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("contact")}} className="hover:text-[#8a7560] cursor-pointer">Contact{menu==="contact"?<hr/>:<></>}</li> 
+            <li onClick={() => setMenu("home")}
+            className={`cursor-pointer hover:text-[#8a7560] ${
+              menu === "home" ? "text-[#8a7560] border-b-2 border-[#8a7560]" : ""
+            }`}>Home</li>
+            <li onClick={() => setMenu("about_us")}
+            className={`cursor-pointer hover:text-[#8a7560] ${
+              menu === "about_us"
+                ? "text-[#8a7560] border-b-2 border-[#8a7560]"
+                : ""
+            }`}>About us</li>
+            <li onClick={() => setMenu("services")}
+            className={`cursor-pointer hover:text-[#8a7560] ${
+              menu === "services"
+                ? "text-[#8a7560] border-b-2 border-[#8a7560]"
+                : ""
+            }`}>Services</li>
+            <li onClick={() => setMenu("contact")}
+            className={`cursor-pointer hover:text-[#8a7560] ${
+              menu === "contact"
+                ? "text-[#8a7560] border-b-2 border-[#8a7560]"
+                : ""
+            }`}>Contact</li> 
         </ul>
-        <div className='nav-login-cart'>
-            <button className="w-40 h-12 rounded-full border border-gray-500 text-gray-700 font-medium text-lg 
-         hover:bg-gray-200 focus:outline-none active:bg-blue-500 active:text-white">Login</button>
-            <img src={cart_icon} alt="carrito" className='cart_icon'/>
-            <div className='nav-cart-count'>0         
-            </div>
+        {/* Login and Cart */}
+        <div className='relative flex items-center gap-4 mt-4 md:mt-0'>
+            <button className="w-32 h-10 rounded-full border border-gray-500 text-gray-700 text-sm hover:bg-gray-200 active:bg-blue-500 active:text-white">
+            Login
+            </button>
+            <img src={cart_icon} alt="Cart" className="w-8 h-8" />
+            <span className="absolute -top-1 right-0 transform translate-x-1 -translate-y-1.5 w-5 h-5 flex items-center justify-center rounded-full bg-red-500 text-white text-xs">
+              0
+            </span>
         </div>
-        </div>
+        
+    </div>
+
+    {/* Mobile Navigation */}
+    <div className="flex md:hidden justify-around items-center mt-4">
+      <button
+          onClick={() => setMenu("home")}
+          className={`flex-1 text-center py-2 ${
+            menu === "home" ? "text-[#8a7560]" : "text-[#181411]"
+          }`}
+      >
+        Home
+      </button>
+      <button
+          onClick={() => setMenu("about_us")}
+          className={`flex-1 text-center py-2 ${
+            menu === "about_us" ? "text-[#8a7560]" : "text-[#181411]"
+          }`}
+      >
+        About Us
+      </button>
+      <button
+        onClick={() => setMenu("services")}
+        className={`flex-1 text-center py-2 ${
+            menu === "services" ? "text-[#8a7560]" : "text-[#181411]"
+          }`}
+      >
+        Services
+      </button>
+      <button
+          onClick={() => setMenu("contact")}
+          className={`flex-1 text-center py-2 ${
+            menu === "contact" ? "text-[#8a7560]" : "text-[#181411]"
+          }`}
+      >
+        Contact
+      </button>
+      </div>
     </div>
   )
 }
