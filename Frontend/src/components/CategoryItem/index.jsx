5dff3a485fe76436f1a8e27f9657c764327f2483
Edward B <ebotelloaroca00@gmail.com>
2025-01-29 21:41:03 -0500

    actualiza fetch de datos desde el back añade categorias

--- Frontend/src/components/CategoryItem/index.jsx
+++ Frontend/src/components/CategoryItem/index.jsx
@@ -4,12 +4,10 @@ const CategoryItem = ({ category }) => {
   return (
     <Link
       className="text-[#181411] font-bold font-sans text-lg bg-[#f5f2f0] py-1 px-3 rounded-xl hover:scale-105 transition-transform duration-200 hover:font-semibold hover:bg-[#ded4cd]"
-      to={`/${category.nombre}`}
+      to={`/${category.encabezado}`}
     >
-      <h3 className="mb-3 capitalize">{category.nombre}</h3>
-      <p className="text-gray-600 text-sm leading-relaxed">
-        {category.descripcion}
-      </p>
+      <h3 className="mb-3 capitalize">{category.encabezado}</h3>
+      <p className="text-gray-600 text-sm leading-relaxed">{category.cuerpo}</p>
     </Link>
   );
 };
