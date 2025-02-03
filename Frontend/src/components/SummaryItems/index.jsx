76466f61b62eb7def4aec80b70e78b46c34d3aff
Edward B <ebotelloaroca00@gmail.com>
2025-02-03 18:56:48 -0500

    diferenciar separador para evitar confuciones

--- Frontend/src/components/SummaryItems/index.jsx
+++ Frontend/src/components/SummaryItems/index.jsx
@@ -1,7 +1,7 @@
 const SummaryItems = ({ item: producto }) => {
   return (
     <li key={producto.id} className="text-[#f5f2f0]">
-      {producto.product.titulo} - {producto.quantity} x $
+      {producto.product.titulo} | {producto.quantity} x $
       {producto.product.precio}
     </li>
   );
